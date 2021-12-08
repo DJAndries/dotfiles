9dfca1f438829f7b84edd0a7848ca28aa53d6453
Darnell Andries <darnell@andries.ca>
2021-12-08 00:30:32 -0800

    Add screenlayout from arandr

---
+++ .screenlayout/main.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 1920x0 --rotate right --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x346 --rotate normal
